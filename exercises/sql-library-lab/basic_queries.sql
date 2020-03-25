0b239748268a700c77821430cc5abeceb06e7817
Najwab <inajjwa2@gmail.com>
2020-03-25 21:54:15 +0300

    solved

--- exercises/sql-library-lab/basic_queries.sql
+++ exercises/sql-library-lab/basic_queries.sql
@@ -5,28 +5,43 @@
 -- SELECT * FROM authors;
 
 -- Get just the name and birth year of all authors
+--  SELECT name, birth_year FROM authors;
 
 -- Get all authors born in the 20th centruy or later
+-- SELECT * FROM authors WHERE birth_year > 1901;
 
 -- Get all authors born in the USA
+--  SELECT * FROM authors WHERE nationality = 'USA';
 
 -- Get all books published on 1985
+--  SELECT * FROM books WHERE publication_date = 1985;
 
 -- Get all books published before 1989
+--SELECT * FROM books WHERE publication_date < 1989;
 
 -- Get just the title of all books.
+-- SELECT title FROM books;
 
 -- Get just the year that 'A Dance with Dragons' was published
   -- Cry when you realize how long it's been
+--  SELECT publication_date FROM books where  title = 'A Dance with Dragons';
 
 -- Get all books which have `the` somewhere in their title (hint, look up LIKE/ILIKE)
+-- SELECT * FROM books where title LIKE '%the%';
 
 -- Add yourself as an author
+-- INSERT INTO authors ( name, nationality, birth_year ) VALUES ( 'Najwa', 'saudi', '1997');
 
 -- Add two books that you'd like to write (you can hard-code your id as the author id)
+-- INSERT INTO books ( title, publication_date, author_id) VALUES ( 'book1', '2020', '9');
+-- INSERT INTO books ( title, publication_date, author_id) VALUES ( 'book2', '2020', '9');
 
 -- Update one of your books to have a new title
+-- update books set title = 'newtitle' WHERE title = 'book1' ;
 
 -- Delete your books
+-- DELETE FROM books WHERE author_id = 9;
 
 -- Delete your author entry
+-- DELETE FROM authors WHERE id = 9;
+ 
