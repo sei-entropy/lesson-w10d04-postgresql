2d1b995e64e1ce7d2f1c3efd993b3c0bebb7f49f
huda <you@example.com>
2020-03-25 19:19:30 +0300

    solve HW

--- exercises/sql-library-lab/basic_queries.sql
+++ exercises/sql-library-lab/basic_queries.sql
@@ -5,28 +5,38 @@
 -- SELECT * FROM authors;
 
 -- Get just the name and birth year of all authors
-
+SELECT name , birth_year FROM authors;
 -- Get all authors born in the 20th centruy or later
+SELECT * FROM authors WHERE birth_year>= 1900;
 
 -- Get all authors born in the USA
+SELECT * FROM authors WHERE nationality = 'United States of America';
 
 -- Get all books published on 1985
+SELECT * FROM books WHERE publication_date = 1985;
 
 -- Get all books published before 1989
+SELECT * FROM books WHERE publication_date < 1989;
 
 -- Get just the title of all books.
-
+SELECT title FROM books;
 -- Get just the year that 'A Dance with Dragons' was published
   -- Cry when you realize how long it's been
-
+SELECT publication_date FROM books WHERE title LIKE '%A Dance with Dragons%';
 -- Get all books which have `the` somewhere in their title (hint, look up LIKE/ILIKE)
-
+SELECT * FROM books WHERE title LIKE '%the%';
 -- Add yourself as an author
-
+INSERT INTO authors ( name, nationality, birth_year ) VALUES ('HUDA' , 'SAUDI' , '8888');
 -- Add two books that you'd like to write (you can hard-code your id as the author id)
+INSERT INTO books ( title,publication_date,author_id) VALUES ('CLOUD' , 2020 , 44);
+INSERT INTO books ( title,publication_date,author_id) VALUES ('SUNSHINE' , 2021 , 44);
 
 -- Update one of your books to have a new title
+ UPDATE books SET title = 'MOON' WHERE title = 'CLOUD';
 
 -- Delete your books
+DELETE FROM books WHERE title = 'MOON';
+DELETE FROM books WHERE title = 'SUNSHINE';
 
 -- Delete your author entry
+ DELETE FROM authors WHERE name = 'HUDA';
