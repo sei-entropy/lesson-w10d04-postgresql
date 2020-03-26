d2fa94a667517bcb7c9047d25c4a9963964a73a8
FaisalAljazaeri <faisal.hassan93@gmail.com>
2020-03-26 03:40:41 +0300

    Finish Advanced Queries

--- exercises/sql-library-lab/advanced_queries.sql
+++ exercises/sql-library-lab/advanced_queries.sql
@@ -2,21 +2,31 @@
 -- For all of these queries, you SHOULD NOT use an id # in a WHERE clause
 
 -- Find all fields (book and author related) for all books written by George R.R. Martin.
+SELECT * FROM books AS b JOIN authors AS a ON b.author_id = a.id  WHERE a.name = 'George R.R. Martin';
 
 -- Find all fields (book and author related) for all books written by Milan Kundera.
+SELECT * FROM books AS b JOIN authors AS a ON b.author_id = a.id  WHERE a.name = 'Milan Kundera';
 
 -- Find all books written by an author from China or the UK.
+SELECT * FROM books AS b JOIN authors AS a ON b.author_id = a.id  WHERE a.nationality = 'United Kingdom' OR a.nationality = 'China';
 
 -- Find out how many books Albert Camus wrote.
+SELECT COUNT(b.id) FROM books AS b JOIN authors AS a ON  b.author_id = a.id WHERE a.name = 'Albert Camus';
 
 -- Find out how many books were written by US authors.
+SELECT COUNT(b.id) FROM books AS b JOIN authors AS a ON  b.author_id = a.id WHERE a.nationality = 'United States of America';
 
 -- Find all books written after 1930 by authors from Argentina.
+SELECT * FROM books AS b JOIN authors AS a ON  b.author_id = a.id WHERE a.nationality = 'Argentina' AND b.publication_date > 1930;
 
 -- Find all books written before 1980 by authors not from the US.
+SELECT * FROM books AS b JOIN authors AS a ON  b.author_id = a.id WHERE a.nationality != 'United States of America' AND b.publication_date < 1980;
 
 -- Find all authors whose names start with 'J'.
+SELECT * FROM authors WHERE lower(name) LIKE 'j%';
 
 -- Find all books whose titles contain 'the'.
+SELECT * FROM books WHERE lower(title) LIKE '%the%';
 
 -- Find all authors who have written books with that start with the letter 'N'.
+SELECT DISTINCT(a.id), a.name, a.nationality, a.birth_year FROM authors AS a JOIN books AS b ON a.id = b.author_id WHERE lower(b.title) LIKE 'n%';
